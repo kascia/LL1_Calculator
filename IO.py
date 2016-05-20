b03a781a876ae083db5d25f1b41b21622596aae4
kascia <lwk92@naver.com>
2016-05-20 10:11:46 +0900

    [add] IO.py

---
+++ IO.py
@@ -0,0 +1,15 @@
+import sys
+
+class IO:
+    def __init__(self):
+        self.strcalc=''
+                
+    def get_strcalc(self):
+        argc = len(sys.argv)
+        for i in range(argc):
+            self.strcalc += argv[i]
+        return strcalc
+    
+    def print_ans(self,ans):
+        return ans
+    
